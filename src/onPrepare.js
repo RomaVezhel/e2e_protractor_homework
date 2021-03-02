fe6c5f625f3a05ef3672d7cefc013eaa7f49b228
Roman.Vezhel <roman.verzhel@clicklogiq.com>
2021-03-02 21:54:34 +0200

    added new reported

--- src/onPrepare.js
+++ src/onPrepare.js
@@ -1,12 +1,16 @@
+const onPrepare = function () {
+  browser.manage().window().maximize();
+  browser.waitForAngularEnabled(false);
 
-const onPrepare = function() {
-    browser
-        .manage()
-        .window()
-        .maximize();
-    browser.waitForAngularEnabled(false);
+  // Adding nice console output.
+  // Provided by: https://github.com/razvanz/jasmine2-reporter
+  let ConsoleReporter = require("jasmine2-reporter").Jasmine2Reporter;
+  let console_reporter_options = {
+    startingSpec: true,
+  };
+  jasmine.getEnv().addReporter(new ConsoleReporter(console_reporter_options));
 };
 
 module.exports = {
-    onPrepare
+  onPrepare,
 };
